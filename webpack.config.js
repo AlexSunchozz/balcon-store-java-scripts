0422104b6c94d286e32896a0a52a8257913d70d6
Alexandr <arepkin96@mail.ru>
2023-10-22 19:57:35 +0300

    change webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,158 @@
-'use strict';
+const path = require("path");
 
-let path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Плагин для работы с HTML
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // Плагин для того, чтобы сборка в папке dist всегда была свежей и файлы не кешировались
+const CopyPlugin = require("copy-webpack-plugin"); // Плагин для копирования папок и файлов
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname + '/src/js/main.js'),
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist/js'
-  },
-  devtool: "source-map",
-  watch: true,
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  config = {
+    // Режим сборки проекта
+    mode: "development",
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', {
-                debug: true,
-                corejs: 3,
-                useBuiltIns: "usage"
-            }]]
-          }
-        }
-      }
-    ]
+    // Входная точка проекта
+    entry: {
+      main: "./src/js/index.js",
+    },
+
+    // Выходная точка проекта
+    output: {
+      filename: "[name].[contenthash].js", // Имя файла после сборки (можно использовать bondle.js, но для корректной сборки обычно используют [name].[contenthash].js для получения уникального имени итогового файла)
+      path: path.resolve(__dirname, "dist"), // Куда сохраняются файлы webpack
+    },
+
+    // Запуск локального сервера и открытие вкладки браузера (изменения в коде автоматически подтягиваются)
+    devServer: {
+      historyApiFallback: true,
+      static: "./",
+      open: true,
+      compress: true,
+      hot: true,
+      port: 8080,
+    },
+
+    // Подключение модулей для обработки различных файлов
+    module: {
+      rules: [
+        // Babel
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "defaults" }]],
+            },
+          },
+        },
+
+        // Файлы стилей
+        {
+          test: /\.css$/i,
+          use: isProduction
+            ? [MiniCssExtractPlugin.loader, "css-loader"]
+            : ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.less$/i,
+          use: isProduction
+            ? [MiniCssExtractPlugin.loader, "css-loader"]
+            : ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: isProduction
+            ? [MiniCssExtractPlugin.loader, "css-loader"]
+            : ["style-loader", "css-loader"],
+        },
+
+        // Файлы изображений
+        {
+          test: /\.(png|jpeg|gif|webp|jpg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "images/[name].[ext]", // Сохраняйте файлы в подпапке images
+              },
+            },
+          ],
+        },
+
+        // С сохранением структуры папок (сохраниться папка src/assets/img/<имя файла>.<расширение>)
+        // {
+        //     test: /\.(png|jpe?g|gif)$/i,
+        //     loader: 'file-loader',
+        //     options: {
+        //         name: '[path][name].[ext]',
+        //     },
+        // },
+
+        // {
+        //   test: /\.(png|jpe?g|gif)$/i,
+        //   type: 'asset/resource',
+        // },
+
+        // Webpack подставит картинки в css, если там будет импорт, но не будет копировать их в папку dist, если картинка меньше 8192 байт
+        // {
+        //   test: /\.(png|jpg|gif)$/i,
+        //   use: [
+        //     {
+        //       loader: 'url-loader',
+        //       options: {
+        //         limit: 8192,
+        //       },
+        //     },
+        //   ],
+        // },
+
+        // Файлы шрифтов
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]", // Сохраняйте файлы с оригинальными именами
+                outputPath: "images", // Укажите только папку, в которую сохранять файлы
+              },
+            },
+          ],
+        },
+        // {
+        //   test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        //   type: 'asset/resource',
+        // },
+      ],
+    },
+
+    // Подключение плагинов
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+        filename: "main.html",
+      }),
+      new CleanWebpackPlugin(),
+      // применять изменения только при горячей перезагрузке
+      new webpack.HotModuleReplacementPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: "./src/assets", to: "assets" },
+          { from: "./src/assets", to: "assets" },
+        ],
+      }),
+    ],
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
   }
+
+  return config;
 };
